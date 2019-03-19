478acda61fc09747076ad689e722207a013714f7
RMH <rory.hennessey@gmail.com>
2019-03-19 17:23:08 -0400

    mvp complete

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -31,4 +31,79 @@ router.get('/:id', (req, res) => {
         })
 })
 
+router.post('/', (req, res) => {
+    const { title, contents } = req.body;
+
+    if (!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+    } else {
+    db 
+        .insert(req.body)
+        .then(post => {
+            res.status(201).json(post);
+        })
+        .catch(error => {
+            res.status(500).json({ error: "There was an error while saving the post to the database" });
+        })
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    db
+        .remove(id)
+        .then(post => {
+            if(post) {
+                res.status(204).end();
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The post information could not be retrieved." });
+        })
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const postInfo = req.body;
+
+    !postInfo.title || !postInfo.contents
+    ? res
+        .status(400)
+        .json({ errorMessage: "Please provide title and contents for the post." })
+    : db 
+        .update(id, postInfo)
+        .then(postBody => {
+            if (postBody === 0) {
+                res
+                .status(404)
+                .json({ message: "The post with the specified ID does not exist." });
+            }
+            db
+                .findById(id)
+                .then(post => {
+                    if (post.length === 0) {
+                        res
+                        .status(404)
+                        .json({ message: "The post with the specified ID could not be located." });
+                    } else {
+                    res
+                    .json(post)
+                    }
+                })
+                .catch(error => {
+                    res
+                        .status(500)
+                        .json({ message: "Error occurred while locating post."})
+                })
+        })
+        .catch (error => {
+            res
+                .status(500)
+                .json({ error: "The post info could not be modified"})
+        })
+
+});
+
 module.exports = router;
